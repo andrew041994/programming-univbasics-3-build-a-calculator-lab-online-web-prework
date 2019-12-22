0724cc2208443343b319ac4b00bd5523da38f046
Alehandro Persaud <andrew041994@gmail.com>
2019-12-22 02:22:35 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+ sum=num1+num2
 end
 
 def subtraction(num1, num2)
-
+subtracts=num1-num2
 end
 
 def division(num1, num2)
-
+divide=num1/num2
 end
 
 def multiplication(num1, num2)
-
+multiply=num2*num1
 end
 
 def modulo(num1, num2)
-
+remainder=num1%num2
 end
 
 def square_root(num)
-
+sqrt=num/9
 end
